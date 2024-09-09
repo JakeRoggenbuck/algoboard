0c1ea6f4b5b60dcca1139b49b865caef6000879f
Jake <jakeroggenbuck2@gmail.com>
2024-09-09 14:57:07 -0700

    Fix syntax

--- frontend/src/Components/Pages/Changelog/Changelog.js
+++ frontend/src/Components/Pages/Changelog/Changelog.js
@@ -34,16 +34,14 @@ export default function Changelog() {
         </header>
 
         <div className="flex flex-col text-white items-center min-h-screen my-4">
-
           <div className="m-4 w-3/5">
             <h1 className="my-4 font-bold text-white text-5xl">
               Changelog for 9-09-2024
             </h1>
 
             <h3 className="my-4 text-white text-2xl">- Change name</h3>
-            <p className="my-4">
-				This is now called the AlgoBoard!
-            </p>
+            <p className="my-4">This is now called the AlgoBoard!</p>
+          </div>
           <div className="m-4 w-3/5">
             <h1 className="my-4 font-bold text-white text-5xl">
               Changelog for 9-08-2024
