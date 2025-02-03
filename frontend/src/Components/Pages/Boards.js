b6554e8e8d664dc46e879dd7fb1256e380b6495e
Jake Roggenbuck <35516367+JakeRoggenbuck@users.noreply.github.com>
2025-02-03 00:30:31 -0800

    Revert "Typescript (#53)"
    This reverts commit 3285787f4603172af18411eca30df888e998a36b.

--- frontend/src/Components/Pages/Boards.js
+++ frontend/src/Components/Pages/Boards.js
@@ -1,7 +1,6 @@
-import BoardList from "../../Components/Elements/BoardList.tsx";
+import BoardList from "../../Components/Elements/BoardList.js";
 import { Link } from "react-router-dom";
-import Feedback from "../../Components/Elements/Feedback.tsx";
-import React from "react";
+import Feedback from "../../Components/Elements/Feedback.js";
 
 function Boards() {
   document.title = "Boards - AlgoBoard";
@@ -29,21 +28,13 @@ function Boards() {
           </div>
 
           <div className="space-x-2">
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://forms.gle/X7PboDVHygyBvTxcA"
-            >
+            <a target="_blank" href="https://forms.gle/X7PboDVHygyBvTxcA">
               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">
                 Invite to My Board
               </button>
             </a>
 
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://forms.gle/X7PboDVHygyBvTxcA"
-            >
+            <a target="_blank" href="https://forms.gle/X7PboDVHygyBvTxcA">
               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">
                 Join New Board
               </button>
