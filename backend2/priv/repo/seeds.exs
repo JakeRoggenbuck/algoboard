f40b3beb47330a482da1850a27788551f92c96df
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2026-01-31 13:05:34 -0800

    Add migration

---
+++ backend2/priv/repo/seeds.exs
@@ -0,0 +1,11 @@
+# Script for populating the database. You can run it as:
+#
+#     mix run priv/repo/seeds.exs
+#
+# Inside the script, you can read and write to any of your
+# repositories directly:
+#
+#     Backend2.Repo.insert!(%Backend2.SomeSchema{})
+#
+# We recommend using the bang functions (`insert!`, `update!`
+# and so on) as they will fail if something goes wrong.
